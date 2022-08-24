2ca39e8b3e71ead81ee91adc7d47e82fa80fa3d7
caoimheD <caoimhe.db5@gmail.com>
2022-08-24 13:31:07 +0000

    test terminal output

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print('Welcome')
+
+question = input('First question?\n A: answer1 B: answer2 C: answer3\n')
+
+if question == 'A':
+    print('Correct!')
+else:
+    print('Incorrect')
