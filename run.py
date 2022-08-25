cf9fbc118a071fb36b36ecb267087e24e37cffc7
caoimheD <caoimhe.db5@gmail.com>
2022-08-25 10:36:36 +0000

    add questions

--- run.py
+++ run.py
@@ -3,10 +3,23 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 print('Welcome')
+score = 0
 
 question = input('First question?\n A: answer1 B: answer2 C: answer3\n')
 
-if question == 'A':
+if question.lower() == 'a':
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
+
+question = input('Second question?\n A: answer1 B: answer2 C: answer3\n')
+
+if question.lower() == 'a':
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect')
+
+print("You got " + str(score) + " questions correct")
+print("You got " + str((score / 2) * 100) + "%")
