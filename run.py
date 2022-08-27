d1a47cf132e6c3355639b45fe902bed26ce060f1
caoimheD <caoimhe.db5@gmail.com>
2022-08-27 19:40:43 +0000

    edit

--- run.py
+++ run.py
@@ -2,24 +2,31 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+from data import Question
+
 print('Welcome')
-score = 0
 
-question = input('First question?\n A: answer1 B: answer2 C: answer3\n')
 
-if question.lower() == 'a':
-    print('Correct!')
-    score += 1
-else:
-    print('Incorrect')
+questiondisplay = [
+    "First question?\n (a): answer1 (b): answer2 (c): answer3\n",
+    "Second question?\n (a): answer1 (b): answer2 (c): answer3\n",
+    "Third question?\n (a): answer1 (b): answer2 (c): answer3\n"
+]
 
-question = input('Second question?\n A: answer1 B: answer2 C: answer3\n')
+questions = [
+    Question(questiondisplay[0], "a"),
+    Question(questiondisplay[1], "b"),
+    Question(questiondisplay[2], "c")
+]
 
-if question.lower() == 'a':
-    print('Correct!')
-    score += 1
-else:
-    print('Incorrect')
+def run_game(questions):
+    score = 0
+    for question in questions:
+        useranswer = input(question.prompt)
+        if useranswer == question.answer:
+            score += 1
 
-print("You got " + str(score) + " questions correct")
-print("You got " + str((score / 2) * 100) + "%")
+    print("You got " + str(score) + " questions correct")
+    print("You got " + str((score / 2) * 100) + "%")
+
+run_game(questions)
